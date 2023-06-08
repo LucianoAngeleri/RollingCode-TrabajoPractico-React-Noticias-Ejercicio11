9d4e8a86acce2d670d9b25f387810d1c65d05cf6
Luciano Angeleri <lcnang@gmail.com>
2023-06-08 12:44:17 -0300

    Se agregó el spinner para la carga de noticias y una imagen que se carga si no eiste una imagen en la noticia

--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -1,10 +1,12 @@
-import { Row, Container } from "react-bootstrap";
+import { Row, Container, Spinner } from "react-bootstrap";
 import Noticia from "./Noticia";
 import { useEffect, useState } from "react";
 
 
 const ListaNoticias = () => {
     const [noticias,setNoticias]= useState([])
+    const [mostrarSpinner, setMostrarSpinner] = useState(true);
+
     const APIkey= "pub_2420662465ebc1a70595767e75a4fe5382b1a"
 
     useEffect(()=>{
@@ -13,15 +15,21 @@ const ListaNoticias = () => {
 
     const consultarAPI = async () =>{
         try {
+            setMostrarSpinner(true);
             const respuesta = await fetch(`https://newsdata.io/api/1/news?apikey=${APIkey}&country=ar&language=es`)
             const dato =  await respuesta.json();
             setNoticias(dato.results);
+            setMostrarSpinner(false);
         } catch (error) {
             console.log(error)
         }
     }
 
-    return (
+    const componenteRenderizado = mostrarSpinner ? (
+        <Container className="my-5 text-center">
+          <Spinner animation="border" variante="primary"></Spinner>
+        </Container>
+      ) : (
         <Container className='py-3'>
         <Row className="g-3">
         {noticias.map((noticia, index) => (
@@ -36,6 +44,9 @@ const ListaNoticias = () => {
             ))}
         </Row>
         </Container>
+      );
+    return (
+        (componenteRenderizado)
     );
 };
 
--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,14 +1,17 @@
 import { Card,Button,Col } from "react-bootstrap";
+import { useState } from "react";
+import imagenNoDisponible from "../assets/imagenNoDisponible.jpg"
 
 const Noticia = ({title,category,description,image_url,link}) => {
+    const imagenCard = (image_url)?(<Card.Img variant="top" src={image_url} />):(<Card.Img variant="top" src={imagenNoDisponible} />)
     return (
         <Col xs={12} sm={12} md={6} lg={3}>
         <Card className="h-100">
-            <Card.Img variant="top" src={image_url} alt={title}></Card.Img>
+            {imagenCard}
             <Card.Body>
                 <Card.Subtitle className="pb-3">{category}</Card.Subtitle>
-                <Card.Title className="pb-3">{title}</Card.Title>
-                <Card.Text className="pb-3">{description}</Card.Text>
+                <Card.Title className="pb-3 text-primary">{title}</Card.Title>
+                <Card.Text className="pb-3 lead fs-6">{description}</Card.Text>
             </Card.Body>
             <Card.Footer className="d-grid">
                 <Button variant="primary" href={link}>Ver noticia completa</Button>
